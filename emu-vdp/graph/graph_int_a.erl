3d676b795cf244957f6096a7f750a815a1d9c2ff
KuznetsovaElizaveta <mrallis01@mail.ru>
2020-11-03 14:06:51 +0300

    Исправление ошибок

--- emu-vdp/graph/graph_int_a.erl
+++ emu-vdp/graph/graph_int_a.erl
@@ -1,19 +1,19 @@
 % Имя модуля.
--module(graph_add).
+-module(graph_int_a).
 
 % Подключаемые файлы.
 -include("defines.hrl").
 -include("records.hrl").
 
 % Экспортируемые функции.
--export([get_graph_add/0]).
+-export([get_graph_int_a/0]).
 
-get_graph_add() ->
+get_graph_int_a() ->
     [
         #command{id = 1, name = iadd, dsts = [{2, 1}]},
         #command{id = 2, name = isub, dsts = [{3, 1}]},
         #command{id = 3, name = imul, dsts = [{4, 2}]},
-        #command{id = 2, name = isub, dsts = [{4, 1}]},
+        %#command{id = 2, name = isub, dsts = [{4, 1}]},
         #command{id = 4, name = mod, dsts = [out]}
         
     ].
--- emu-vdp/tokens/tokens_int_a.erl
+++ emu-vdp/tokens/tokens_int_a.erl
@@ -6,20 +6,21 @@
 -include("records.hrl").
 
 % Экспортируемые функции.
--export([get_tokens_add/0]).
+-export([get_tokens_int_a/0]).
 
 
-get_tokens_add() ->
+get_tokens_int_a() ->
 
     % Коэффициенты квадратного уравнения.
     A = 1,
     B = -3,
-    %C = 2.0,
+    C = 2,
 
     [
         #token{command_id = 1, state = default, entry = 1, data = {int, 4}},
         #token{command_id = 1, state = default, entry = 2, data = {int, A}},
         #token{command_id = 2, state = default, entry = 2, data = {int, 3}},
-        #token{command_id = 3, state = default, entry = 2, data = {int, B}}
+        #token{command_id = 3, state = default, entry = 2, data = {int, B}},
+        #token{command_id = 4, state = default, entry = 2, data = {int, C}}
         
     ].
