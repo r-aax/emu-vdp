c5403246765b498c830975a44763247d94767868
KuznetsovaElizaveta <mrallis01@mail.ru>
2020-11-03 16:18:49 +0300

    Исправление ошибок

--- emu-vdp/graph/graph_int_a.erl
+++ emu-vdp/graph/graph_int_a.erl
@@ -10,10 +10,15 @@
 
 get_graph_int_a() ->
     [
-        #command{id = 1, name = iadd, dsts = [{2, 1}]},
-        #command{id = 2, name = isub, dsts = [{3, 1}]},
-        #command{id = 3, name = imul, dsts = [{4, 2}]},
+        #command{id = 1, name = iadd, dsts = [2, 1]},
+        #command{id = 2, name = isub, dsts = [out]}
+        
+        
+        %#command{id = 1, name = iadd, dsts = [out]}
+        %#command{id = 1, name = iadd, dsts = [{2, 1}]},
+        %#command{id = 2, name = isub, dsts = [{3, 1}]},
+        %#command{id = 3, name = imul, dsts = [{4, 2}]},
         %#command{id = 2, name = isub, dsts = [{4, 1}]},
-        #command{id = 4, name = mod, dsts = [out]}
+        %#command{id = 4, name = mod, dsts = [out]}
         
     ].
